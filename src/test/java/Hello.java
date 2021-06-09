1b802f7d6219b9e92fa8de2204efe294878807d5
onurcan-ada <sleepiaapp@gmail.com>
2021-06-09 12:12:59 +0300

    added more hello feature

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("LOcal repo is already published to Github");
+        System.out.println("added more work");
     }
 }
